f37db9ecbdc6a0e9cb68c5573eff964697a92975
Dynna <ciorbadina9@gmail.com>
2021-03-11 22:21:59 +0200

    remove unused comments

--- MyApplication/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -9,8 +9,6 @@ import com.example.myapplication.data.Datasource
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
-   // private lateinit var posts: MutableList<Post>
-  //  val items: ArrayList<Post> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -21,32 +19,5 @@ class HomeActivity : AppCompatActivity() {
        recyclerView.adapter = ItemAdapter(this, myDataset)
 
        recyclerView.setHasFixedSize(true)
-
-       // ------------------------------------------------------
-
-      // create layout file which represents one post - done
-      // create data source - done
-     // posts = mutableListOf()
-      // create adapter
-      // bind the adapter and layout manager to the rv
-
-
-/*        addItems()
-
-       // feedList.layoutManager = LinearLayoutManager(this)
-
-        // for multiple columns
-         feedList.layoutManager = GridLayoutManager(this, 2)
-
-        feedList.adapter = ItemAdapter(items, this)*/
-
     }
-
-/*    fun addItems() {
-        items.add(Post("user1", "imageUrl"))
-        items.add(Post("user2","imageUrl2"))
-        items.add(Post("user3","imageUrl3"))
-      //  items.add("https://images.unsplash.com/photo-1502912274126-08f31a73a5f0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=729&q=80")
-    }*/
-
 }
