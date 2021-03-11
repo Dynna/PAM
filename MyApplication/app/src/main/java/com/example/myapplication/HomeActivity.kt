f6bba3cbc7be7ad3f830f380b8b100ae1271abe7
Dynna <ciorbadina9@gmail.com>
2021-03-11 23:30:43 +0200

    access view from view binding

--- MyApplication/app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -6,18 +6,22 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.adapter.ItemAdapter
 import com.example.myapplication.data.Datasource
+import com.example.myapplication.databinding.ActivityHomeBinding
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-       val myDataset = Datasource().loadPosts()
+        val myDataset = Datasource().loadPosts()
 
-       val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-       recyclerView.adapter = ItemAdapter(this, myDataset)
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
 
-       recyclerView.setHasFixedSize(true)
+        recyclerView.setHasFixedSize(true)
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/adapter/ItemAdapter.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/adapter/ItemAdapter.kt
@@ -8,34 +8,34 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ListItemBinding
 import com.example.myapplication.model.Post
+import kotlinx.android.synthetic.main.list_item.view.*
 
 class ItemAdapter(
     private val context: Context,
     private val dataset: List<Post>
     ): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+    private lateinit var binding: ListItemBinding
 
-    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
-        val textView_2: TextView = view.findViewById(R.id.item_author)
-        val textView: TextView = view.findViewById(R.id.item_title)
-        val imageView: ImageView = view.findViewById(R.id.item_image)
-    }
+    inner class ItemViewHolder(val binding: ListItemBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        // create a new view
-        val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item, parent, false)
 
-        return ItemViewHolder(adapterLayout)
+        return ItemViewHolder(
+            ListItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item = dataset[position]
-        holder.textView_2.text = context.resources.getString(item.strResourceId)
-        holder.textView.text = context.resources.getString(item.stringResourceId)
-        holder.imageView.setImageResource(item.imageResourceId)
+        holder.binding.itemAuthor.setText(dataset[position].strResourceId)
+        holder.binding.itemTitle.setText(dataset[position].stringResourceId)
+        holder.binding.itemImage.setImageResource(dataset[position].imageResourceId)
     }
 
     override fun getItemCount() = dataset.size
-
 }
