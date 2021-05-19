41c16aa3ff4e552c5bfc7145cb0f30ef720c42c1
Dynna <ciorbadina9@gmail.com>
2021-05-19 15:47:53 +0300

    add unit tests for main view model

---
+++ MyApplication/app/src/test/java/com/example/myapplication/data/repo/MainRepositoryTest.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication.data.repo
+
+import com.example.myapplication.data.api.ApiHelper
+import com.example.myapplication.data.api.ApiService
+import com.example.myapplication.data.model.User
+import org.hamcrest.CoreMatchers.any
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.mockito.Mockito.mock
+
+class MainRepositoryTest {
+
+    
+    @Test
+    fun getUsers() {
+    }
+}
